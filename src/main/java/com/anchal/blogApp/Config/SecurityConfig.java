e055df601bc5a84e2f3ada97bcdf265346ffaef1
anchalvatre <vatreanchal@gmail.com>
2025-06-26 23:13:56 +0530

    Adding Security

---
+++ src/main/java/com/anchal/blogApp/Config/SecurityConfig.java
@@ -0,0 +1,33 @@
+package com.anchal.blogApp.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.net.http.HttpRequest;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.
+                csrf(csrf -> csrf.disable()).
+                authorizeHttpRequests(auth -> auth.
+                        requestMatchers(HttpMethod.GET, "/api/v1/categories/**").authenticated().
+                        anyRequest().authenticated()).
+                sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+
+
+        return http.build();
+    }
+
+
+}
